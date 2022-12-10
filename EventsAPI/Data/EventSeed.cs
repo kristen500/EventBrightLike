72d461d9768b4adf0d8d3c2409541fbd41cf2c62
kristen500 <115898319+kristen500@users.noreply.github.com>
2022-12-10 13:54:39 -0800

    corrections

--- EventsAPI/Data/EventSeed.cs
+++ EventsAPI/Data/EventSeed.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace EventsAPI.Data
-   
+
 {
     public static class EventSeed
     {
@@ -13,37 +13,38 @@ namespace EventsAPI.Data
         {
             context.Database.Migrate();
 
-            if (!context.EventItem.Any())
-            {
-                context.EventItem.AddRange(GetEventItems());
-                context.SaveChanges();
-
-                if (!context.EventOrganizers.Any())
-            {
-                context.EventOrganizers.AddRange(GetEventOrganizers());
-                context.SaveChanges();
-            }
             if (!context.EventTypes.Any())
             {
                 context.EventTypes.AddRange(GetEventTypes());
                 context.SaveChanges();
             }
 
-        }
-        }
-
-        private static IEnumerable<EventItem> GetEventItems()
-
-        {
-            return new List<EventItem>
+            if (!context.EventOrganizers.Any())
             {
-            new EventItem { EventTypeId = 1, EventOrganizerId = 1, Description = "Everyone is Santa all over Downtown Seattle to Drink, eat, dance, and be merry", Name = "SantaCon", Price = 15, PictureUrl = "" },
-            new EventItem { EventTypeId = 2, EventOrganizerId = 2, Description = "5k Run", Name = "ItsaRunderfulLife", Price = 25, PictureUrl = "" },
-            new EventItem { EventTypeId = 3, EventOrganizerId = 3, Description = "80s Party", Name = "NiteWave", Price = 20, PictureUrl = "" },
-            new EventItem { EventTypeId = 4, EventOrganizerId = 4, Description = "Enjoy handcrafted cocktails while exploring boutique shops ", Name = "BallardCocktailTrail", Price = 45, PictureUrl = "" },
-        };
-    }
-           
+                context.EventOrganizers.AddRange(GetEventOrganizers());
+                context.SaveChanges();
+            }
+
+            if (!context.EventItem.Any())
+            {
+                context.EventItem.AddRange(GetEventItems());
+                context.SaveChanges();
+
+            }
+
+        }
+
+        private static IEnumerable<EventType> GetEventTypes()
+        {
+            return new List<EventType>
+            {
+                 new EventType{Type="Holiday"},
+                new EventType{Type="Sports&Fitness"},
+                new EventType{Type="Music"},
+                new EventType{Type="Food&Drink"}
+
+            };
+        }
 
         private static IEnumerable<EventOrganizer> GetEventOrganizers()
         {
@@ -57,15 +58,20 @@ namespace EventsAPI.Data
             };
         }
 
-        private static IEnumerable<EventType> GetEventTypes()
+
+        private static IEnumerable<EventItem> GetEventItems()
         {
-            return new List<EventType>
+            return new List<EventItem>
             {
-                new EventType{Type="Holiday"},
-                new EventType{Type="Sports&Fitness"},
-                new EventType{Type="Music"},
-                new EventType{Type="Food&Drink"}
+
+                new EventItem { EventTypeId = 1, EventOrganizerId = 1, Description = "Everyone is Santa all over Downtown Seattle to Drink, eat, dance, and be merry", Name = "SantaCon", Price = 15, PictureUrl = "" },
+            new EventItem { EventTypeId = 2, EventOrganizerId = 2, Description = "5k Run", Name = "ItsaRunderfulLife", Price = 25, PictureUrl = "" },
+            new EventItem { EventTypeId = 3, EventOrganizerId = 3, Description = "80s Party", Name = "NiteWave", Price = 20, PictureUrl = "" },
+            new EventItem { EventTypeId = 4, EventOrganizerId = 4, Description = "Enjoy handcrafted cocktails while exploring boutique shops ", Name = "BallardCocktailTrail", Price = 45, PictureUrl = "" },
+
             };
         }
     }
 }
+     
+  
