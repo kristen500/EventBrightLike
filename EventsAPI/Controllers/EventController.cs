edb3d0ca73eb51d0faee7bdc182cb47f6c998c45
kristen500 <115898319+kristen500@users.noreply.github.com>
2022-12-18 08:38:52 -0800

    updates

--- EventsAPI/Controllers/EventController.cs
+++ EventsAPI/Controllers/EventController.cs
@@ -1,4 +1,5 @@
 ﻿using EventsAPI.Data;
+using EventsAPI.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,9 +11,11 @@ namespace EventsAPI.Controllers
     public class EventController : ControllerBase
     {
         private readonly EventContext _context;
-        public EventController(EventContext context)
+        private readonly IConfiguration _config;
+        public EventController(EventContext context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         [HttpGet("[action]")]
@@ -26,6 +29,29 @@ namespace EventsAPI.Controllers
         {
             var organizers = await _context.EventOrganizers.ToListAsync();
             return Ok(organizers);
+        }
+
+        [HttpGet("[action]")]
+        public async Task<IActionResult> Items(
+            [FromQuery]int pageIndex = 0, [FromQuery]int pageSize = 4)
+            {
+             var items = await _context.EventItem
+              .OrderBy(e => e.Name)
+              .Skip(pageIndex * pageSize)
+              .Take(pageSize)
+              .ToListAsync();
+
+            items = ChangePictureUrl(items);
+            return Ok(items);
+        }
+
+        private List<EventItem> ChangePictureUrl(List<EventItem> items)
+        {
+            items.ForEach(item => item.PictureUrl = item.PictureUrl
+                            .Replace("http://externaleventbaseurltobereplaced",
+                            _config["ExternalBaseUrl"]));
+
+            return items;
         }
     }
 }
